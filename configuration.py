7cb71bac7a8f59decccbe74865f7725471a5a43a
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-09-10 12:18:54 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -8,32 +8,26 @@ experiment_name = 'test1'
 
 # Model settings Unet2D
 weight_init = 'he_normal'    # xavier_uniform/ xavier_normal/ he_normal /he_uniform /caffe_uniform/ simple/ bilinear
-#model_handle = model_structure.unet2D_same_mod
-#model_handle = model_structure.Dunet2D_same_mod
-
+model_handle = model_structure.unet2D_same
+#model_handle = model_structure.ResUNet
+#model_handle = model_structure.DenseUNet
+#model_handle = model_structure.net1
 
 # Data settings
 data_mode = '2D' 
-image_size = (192, 192)   #(212,212)
-target_resolution = (1, 1)
-pixel_size = (1,1) 
+image_size = (192, 192)
 nlabels = 4
-split_test_train = True   #divide patients in train and test. If true define split
-split = 5                 #  2: 50% train and 50% validation,    5: 80% training, 20% validation
 train_on_all_data = False 
 gt_exists = True    #True if it exists the ground_trth images, otherwise False.
-z_dim = 8
 
 # Training settings
-batch_size = 4      #4 
-learning_rate = 0.001   #unet: 0.01    enet: 0.0005
+batch_size = 4 
+learning_rate = 0.001
 optimizer_handle = tf.compat.v1.train.AdamOptimizer     #(beta1 = 0.9, beta2 = 0.999, epsilon=1e-08)
-schedule_lr = False    #decrease 10 times the LR when loss gradient lower than threshold
-weight_decay = 0  # enet:2e-4    #unet: 0.00000
+schedule_lr = False       #decrease 10 times the LR when loss gradient lower than threshold
+weight_decay = 0 
 momentum = None
-# loss can be 'weighted_crossentropy'/'crossentropy'/'dice'/'dice_onlyfg'/
-# 'crossentropy_and_dice (alfa,beta)'/'tversky'/'focal_tversky'
-loss_type = 'crossentropy_and_dice'
+loss_type = 'crossentropy_and_dice'     #'weighted_crossentropy'/'crossentropy'/'dice'/'dice_onlyfg'/'crossentropy_and_dice (alfa,beta)'/'tversky'/'focal_tversky' --> https://arxiv.org/pdf/2006.14822.pdf
 alfa = 1     #1      
 beta = 0.4   #1      
 augment_batch = True
@@ -50,10 +44,6 @@ test_data_root = 'F:\ARTEFACTS'
 project_root = 'F:\ARTEFACTS'                       
 log_root = os.path.join(project_root, 'artefact_logdir')
 weights_root = os.path.join(log_root, experiment_name)
-
-# Pre-process settings
-standardize = False
-normalize = True
 
 # Rarely changed settings
 max_epochs = 1000
