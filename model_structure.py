5cc69f0acaf72f3a549c467bbaabf0fdd6ffba4a
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-09-20 10:10:49 +0200

    Update model_structure.py

--- model_structure.py
+++ model_structure.py
@@ -1,6 +1,7 @@
+import tensorflow as tf
+
 from tfwrapper import layers
 
-slim = tf.contrib.slim
 import logging
 
 
@@ -58,7 +59,7 @@ def unet2D_same(images, training, nlabels):
     logging.info('conv5_2')
     logging.info(conv5_2.shape)
     
-    upconv4 = layers.Upsample(conv5_2)
+    upconv4 = layers.upsample(conv5_2)
     #upconv4 = layers.deconv2D_layer_bn(conv5_2, name='upconv4', kernel_size=(4, 4), strides=(2, 2), num_filters=512, weight_init='bilinear', training=training)
     logging.info('upconv4')
     logging.info(upconv4.shape)
@@ -73,7 +74,7 @@ def unet2D_same(images, training, nlabels):
     logging.info('conv6_2')
     logging.info(conv6_2.shape)
     
-    upconv3 = layers.Upsample(conv6_2)
+    upconv3 = layers.upsample(conv6_2)
     #upconv3 = layers.deconv2D_layer_bn(conv6_2, name='upconv3', kernel_size=(4, 4), strides=(2, 2), num_filters=256, weight_init='bilinear', training=training)
     logging.info('upconv3')
     logging.info(upconv3.shape)
@@ -88,7 +89,7 @@ def unet2D_same(images, training, nlabels):
     logging.info('conv7_2')
     logging.info(conv7_2.shape)
 
-    upconv2 = layers.Upsample(conv7_2)
+    upconv2 = layers.upsample(conv7_2)
     #upconv2 = layers.deconv2D_layer_bn(conv7_2, name='upconv2', kernel_size=(4, 4), strides=(2, 2), num_filters=128, weight_init='bilinear', training=training)
     logging.info('upconv2')
     logging.info(upconv2.shape)
@@ -103,7 +104,7 @@ def unet2D_same(images, training, nlabels):
     logging.info('conv8_2')
     logging.info(conv8_2.shape)
 
-    upconv1 = layers.Upsample(conv8_2)
+    upconv1 = layers.upsample(conv8_2)
     #upconv1 = layers.deconv2D_layer_bn(conv8_2, name='upconv1', kernel_size=(4, 4), strides=(2, 2), num_filters=64, weight_init='bilinear', training=training)
     logging.info('upconv1')
     logging.info(upconv1.shape)
@@ -140,15 +141,15 @@ def ResUNet(images, training, nlabels):
     b0 = layers.residual_block(e3, 'b0', num_filters=512, strides=[2,1], training=training)
     
     #decoder
-    up3 = layers.Upsample(b0)
+    up3 = layers.upsample(b0)
     concat3 = tf.concat([up3, e3], axis=3,  name='concat3')
     d3 = layers.residual_block(concat3, 'd3', num_filters=256, strides=[1,1], training=training)
     
-    up2 = layers.Upsample(d3)
+    up2 = layers.upsample(d3)
     concat2 = tf.concat([up2, e2], axis=3,  name='concat2')
     d2 = layers.residual_block(concat2, 'd2', num_filters=128, strides=[1,1], training=training)
     
-    up1 = layers.Upsample(d2)
+    up1 = layers.upsample(d2)
     concat1 = tf.concat([up1, e1], axis=3,  name='concat1')
     d1 = layers.residual_block(concat1, 'd1', num_filters=64, strides=[1,1], training=training)
     
@@ -182,23 +183,23 @@ def DenseUNet(images, training, nlabels):
     dens4 = layers.dense_block(trans3, 'dens4', growth_rate=32, n_layers=16, training=training)
 
     #decoder
-    up1 = layers.Upsample(dens4)
+    up1 = layers.upsample(dens4)
     concat1 = tf.concat([up1, dens3], axis=3, name='concat1')
     conv2 = layers.conv2D_layer_bn(concat1, 'conv2', num_filters=640, kernel_size=(3,3), training=training)
     
-    up2 = layers.Upsample(conv2)
+    up2 = layers.upsample(conv2)
     concat2 = tf.concat([up2, dens2], axis=3, name='concat2')
     conv3 = layers.conv2D_layer_bn(concat2, 'conv3', num_filters=256, kernel_size=(3,3), training=training)
 
-    up3 = layers.Upsample(conv3)
+    up3 = layers.upsample(conv3)
     concat3 = tf.concat([up3, dens1], axis=3, name='concat3')
     conv4 = layers.conv2D_layer_bn(concat3, 'conv4', num_filters=64, kernel_size=(3,3), training=training)
     
-    up4 = layers.Upsample(conv4)
+    up4 = layers.upsample(conv4)
     concat4 = tf.concat([up4, conv1], axis=3, name='concat4')
     conv5 = layers.conv2D_layer_bn(concat4, 'conv5', num_filters=64, kernel_size=(3,3), training=training)
     
-    up5 = layers.Upsample(conv5)
+    up5 = layers.upsample(conv5)
     conv6 = layers.conv2D_layer_bn(up5, 'conv6', num_filters=48, kernel_size=(3,3), training=training)
     
     pred = layers.conv2D_layer_bn(conv6, 'pred', num_filters=nlabels, kernel_size=(1,1), activation=tf.identity, training=training)
@@ -210,6 +211,7 @@ def DenseUNet(images, training, nlabels):
 def net1(images, training, nlabels):
 
     #encoder
+    print('images', images.shape)
     select1 = layers.selective_kernel_block(images, 'select1', num_filters=32, training=training)
     print('select1', select1.shape)
     
@@ -249,48 +251,48 @@ def net1(images, training, nlabels):
     print('b2', b2.shape)
     
     #decoder    
-    up4 = layers.Upsample(b2)
+    up4 = layers.upsample(b2)
     print('up4', up4.shape)
     
     att4 = layers.spatial_attention(dens4, 'att4')
     print('att4', att4.shape)
-    proj4 = layers.projection(att4, 'proj4', num_filters=192)
+    proj4 = layers.projection(att4, 'proj4', num_filters=192, training=training)
     print('proj4', proj4.shape)
     c4 = tf.concat([proj4, up4], axis=3, name='c4')
     print('c4', c4.shape)
     conv4= layers.conv2D_layer_bn(c4, 'conv4', num_filters=192, training=training)
     print('conv4', conv4.shape)
     
-    up3 = layers.Upsample(conv4)
+    up3 = layers.upsample(conv4)
     print('up3', up3.shape)
     
     att3 = layers.spatial_attention(dens3, 'att3')
     print('att3', att3.shape)
-    proj3 = layers.projection(att3, 'proj3', num_filters=96)
+    proj3 = layers.projection(att3, 'proj3', num_filters=96, training=training)
     print('proj3', proj3.shape)
     c3 = tf.concat([proj3, up3], axis=3, name='c3')
     print('c3', c3.shape)
     conv3= layers.conv2D_layer_bn(c3, 'conv3', num_filters=96, training=training)
     print('conv3', conv3.shape)
     
-    up2 = layers.Upsample(conv3)
+    up2 = layers.upsample(conv3)
     print('up2', up2.shape)
     
     att2 = layers.spatial_attention(dens2, 'att2')
     print('att2', att2.shape)
-    proj2 = layers.projection(att2, 'proj2', num_filters=48)
+    proj2 = layers.projection(att2, 'proj2', num_filters=48, training=training)
     print('proj2', proj2.shape)
     c2 = tf.concat([proj2, up2], axis=3, name='c2')
     print('c2', c2.shape)
     conv2= layers.conv2D_layer_bn(c2, 'conv2', num_filters=48, training=training)
     print('conv2', conv2.shape)
     
-    up1 = layers.Upsample(conv2)
+    up1 = layers.upsample(conv2)
     print('up1', up1.shape)
     
     att1 = layers.spatial_attention(select1, 'att1')
     print('att1', att1.shape)
-    proj1 = layers.projection(att1, 'proj1', num_filters=32)
+    proj1 = layers.projection(att1, 'proj1', num_filters=32, training=training)
     print('proj1', proj1.shape)
     c1 = tf.concat([proj1, up1], axis=3, name='c1')
     print('c1', c1.shape)
@@ -298,7 +300,7 @@ def net1(images, training, nlabels):
     print('conv1', conv1.shape)
     
     #deep supervision
-    up3d = layers.Upsample(conv4)
+    up3d = layers.upsample(conv4)
     print('up3d', up3d.shape)
     conv3d = layers.conv2D_layer_bn(up3d, 'conv3d', num_filters=96, kernel_size=(1,1), training=training)
     print('conv3d', conv3d.shape)
@@ -306,7 +308,7 @@ def net1(images, training, nlabels):
     sum3 = tf.add(conv3d, conv3)
     print('sum3', sum3.shape)
     
-    up2d = layers.Upsample(sum3)
+    up2d = layers.upsample(sum3)
     print('up2d', up2d.shape)
     conv2d = layers.conv2D_layer_bn(up2d, 'conv2d', num_filters=48, kernel_size=(1,1), training=training)
     print('conv2d', conv2d.shape)
@@ -314,7 +316,7 @@ def net1(images, training, nlabels):
     sum2 = tf.add(conv2d, conv2)
     print('sum2', sum2.shape)
     
-    up1d = layers.Upsample(sum2)
+    up1d = layers.upsample(sum2)
     print('up1d', up1d.shape)
     conv1d = layers.conv2D_layer_bn(up1d, 'conv1d', num_filters=32, kernel_size=(1,1), training=training)
     print('conv1d', conv1d.shape)
