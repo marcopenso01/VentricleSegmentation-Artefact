42621da13808df5c403fa9882e4714860bbb94df
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-11-24 09:17:55 +0100

    Update model_structure.py

--- model_structure.py
+++ model_structure.py
@@ -720,3 +720,377 @@ def mod5unet2D(images, training, nlabels):
                                   training=training)
 
     return pred
+
+
+def mod6unet2D(images, training, nlabels):
+    n_filt = 48
+    conv1_1 = layers.conv2D_layer_bn(images, 'conv1_1', num_filters=n_filt, training=training)
+    conv1_2 = layers.conv2D_layer_bn(conv1_1, 'conv1_2', num_filters=n_filt, training=training)
+
+    pool1 = layers.max_pool_layer2d(conv1_2)
+    #pool1 = layers.dropout_layer(pool1, 'drop1', training=training, keep_prob=0.2)
+
+    conv2_1 = layers.conv2D_layer_bn(pool1, 'conv2_1', num_filters=n_filt * 2, training=training)
+    conv2_2 = layers.conv2D_layer_bn(conv2_1, 'conv2_2', num_filters=n_filt * 2, training=training)
+
+    pool2 = layers.max_pool_layer2d(conv2_2)
+    #pool2 = layers.dropout_layer(pool2, 'drop2', training=training, keep_prob=0.2)
+
+    conv3_1 = layers.conv2D_layer_bn(pool2, 'conv3_1', num_filters=n_filt * 4, training=training)
+    conv3_2 = layers.conv2D_layer_bn(conv3_1, 'conv3_2', num_filters=n_filt * 4, training=training)
+
+    pool3 = layers.max_pool_layer2d(conv3_2)
+    #pool3 = layers.dropout_layer(pool3, 'drop3', training=training, keep_prob=0.2)
+
+    conv4_1 = layers.conv2D_layer_bn(pool3, 'conv4_1', num_filters=n_filt * 8, training=training)
+    conv4_2 = layers.conv2D_layer_bn(conv4_1, 'conv4_2', num_filters=n_filt * 8, training=training)
+
+    pool4 = layers.max_pool_layer2d(conv4_2)
+    #pool4 = layers.dropout_layer(pool4, 'drop4', training=training, keep_prob=0.2)
+
+    conv5_1 = layers.conv2D_layer_bn(pool4, 'conv5_1', num_filters=n_filt * 16, training=training)
+    conv5_2 = layers.conv2D_layer_bn(conv5_1, 'conv5_2', num_filters=n_filt * 16, training=training)
+
+    deconv4 = layers.deconv2D_layer_bn(conv5_2, 'deconv4', num_filters=4, training=training)
+
+    att4 = layers.attention(conv4_2, 'att4', conv5_2)
+    concat4 = tf.concat([att4, deconv4], axis=-1, name='concat4')
+
+    conv6_1 = layers.conv2D_layer_bn(concat4, 'conv6_1', num_filters=n_filt * 8, training=training)
+
+    deconv3 = layers.deconv2D_layer_bn(conv6_1, 'deconv3', num_filters=4, training=training)
+
+    att3 = layers.attention(conv3_2, 'att3', conv6_1)
+    concat3 = tf.concat([att3, deconv3], axis=-1, name='concat3')
+
+    conv7_1 = layers.conv2D_layer_bn(concat3, 'conv7_1', num_filters=n_filt * 4, training=training)
+
+    deconv2 = layers.deconv2D_layer_bn(conv7_1, 'deconv2', num_filters=4, training=training)
+
+    att2 = layers.conv2D_layer_bn(conv2_2, 'att2', kernel_size=(1, 1), num_filters=n_filt*2, training=training)
+    concat2 = tf.concat([att2, deconv2], axis=-1, name='concat2')
+
+    conv8_1 = layers.conv2D_layer_bn(concat2, 'conv8_1', num_filters=n_filt * 2, training=training)
+
+    deconv1 = layers.deconv2D_layer_bn(conv8_1, 'deconv1', num_filters=4, training=training)
+
+    att1 = layers.conv2D_layer_bn(conv1_2, 'att1', kernel_size=(1, 1), num_filters=n_filt, training=training)
+    concat1 = tf.concat([att1, deconv1], axis=-1, name='concat1')
+
+    conv9_1 = layers.conv2D_layer_bn(concat1, 'conv9_1', num_filters=n_filt, training=training)
+
+    deep2 = layers.upsample(conv7_1)
+    add2 = tf.concat([deep2, conv8_1], axis=-1, name='add2')
+    deep1 = layers.upsample(add2)
+    add1 = tf.concat([deep1, conv9_1], axis=-1, name='add1')
+
+    pred = layers.conv2D_layer_bn(add1, 'pred', num_filters=nlabels, kernel_size=(1, 1), activation=tf.identity,
+                                  training=training)
+
+    print('conv1_1', conv1_1.shape)
+    print('conv1_2', conv1_2.shape)
+    print('pool1', pool1.shape)
+    print('conv2_1', conv2_1.shape)
+    print('conv2_2', conv2_2.shape)
+    print('pool2', pool2.shape)
+    print('conv3_1', conv3_1.shape)
+    print('conv3_2', conv3_2.shape)
+    print('pool3', pool3.shape)
+    print('conv4_1', conv4_1.shape)
+    print('conv4_2', conv4_2.shape)
+    print('pool4', pool4.shape)
+    print('conv5_1', conv5_1.shape)
+    print('conv5_2', conv5_2.shape)
+    print('deconv4', deconv4.shape)
+    print('att4', att4.shape)
+    print('concat4', concat4.shape)
+    print('conv6_1', conv6_1.shape)
+    print('deconv3', deconv3.shape)
+    print('att3', att3.shape)
+    print('concat3', concat3.shape)
+    print('conv7_1', conv7_1.shape)
+    print('deconv2', deconv2.shape)
+    print('att2', att2.shape)
+    print('concat2', concat2.shape)
+    print('conv8_1', conv8_1.shape)
+    print('deconv1', deconv1.shape)
+    print('att1', att1.shape)
+    print('concat1', concat1.shape)
+    print('conv9_1', conv9_1.shape)
+    print('deep2', deep2.shape)
+    print('add2', add2.shape)
+    print('deep1', deep1.shape)
+    print('add1', add1.shape)
+    print('pred', pred.shape)
+
+    return pred
+
+
+def mod7unet2D(images, training, nlabels):
+    n_filt = 48
+    conv1_1 = layers.conv2D_layer_bn(images, 'conv1_1', num_filters=n_filt, training=training)
+    conv1_2 = layers.conv2D_layer_bn(conv1_1, 'conv1_2', num_filters=n_filt, training=training)
+
+    pool1 = layers.max_pool_layer2d(conv1_2)
+    pool1 = layers.dropout_layer(pool1, 'drop1', training=training, keep_prob=0.2)
+
+    conv2_1 = layers.conv2D_layer_bn(pool1, 'conv2_1', num_filters=n_filt * 2, training=training)
+    conv2_2 = layers.conv2D_layer_bn(conv2_1, 'conv2_2', num_filters=n_filt * 2, training=training)
+
+    pool2 = layers.max_pool_layer2d(conv2_2)
+    pool2 = layers.dropout_layer(pool2, 'drop2', training=training, keep_prob=0.2)
+
+    conv3_1 = layers.conv2D_layer_bn(pool2, 'conv3_1', num_filters=n_filt * 4, training=training)
+    conv3_2 = layers.conv2D_layer_bn(conv3_1, 'conv3_2', num_filters=n_filt * 4, training=training)
+
+    pool3 = layers.max_pool_layer2d(conv3_2)
+    pool3 = layers.dropout_layer(pool3, 'drop3', training=training, keep_prob=0.2)
+
+    conv4_1 = layers.conv2D_layer_bn(pool3, 'conv4_1', num_filters=n_filt * 8, training=training)
+    conv4_2 = layers.conv2D_layer_bn(conv4_1, 'conv4_2', num_filters=n_filt * 8, training=training)
+
+    pool4 = layers.max_pool_layer2d(conv4_2)
+    pool4 = layers.dropout_layer(pool4, 'drop4', training=training, keep_prob=0.2)
+
+    conv5_1 = layers.conv2D_layer_bn(pool4, 'conv5_1', num_filters=n_filt * 16, training=training)
+    conv5_2 = layers.conv2D_layer_bn(conv5_1, 'conv5_2', num_filters=n_filt * 16, training=training)
+
+    deconv4 = layers.upsample(conv5_2)
+
+    att4 = layers.attention(conv4_2, 'att4', conv5_2)
+    concat4 = tf.concat([att4, deconv4], axis=-1, name='concat4')
+
+    conv6_1 = layers.conv2D_layer_bn(concat4, 'conv6_1', num_filters=n_filt * 8, training=training)
+    conv6_2 = layers.conv2D_layer_bn(conv6_1, 'conv6_2', num_filters=n_filt * 8, training=training)
+
+    deconv3 = layers.upsample(conv6_2)
+
+    att3 = layers.attention(conv3_2, 'att3', conv6_1)
+    concat3 = tf.concat([att3, deconv3], axis=-1, name='concat3')
+
+    conv7_1 = layers.conv2D_layer_bn(concat3, 'conv7_1', num_filters=n_filt * 4, training=training)
+    conv7_2 = layers.conv2D_layer_bn(conv7_1, 'conv7_2', num_filters=n_filt * 4, training=training)
+
+    deconv2 = layers.upsample(conv7_2)
+
+    att2 = layers.conv2D_layer_bn(conv2_2, 'att2', kernel_size=(1, 1), num_filters=n_filt*2, training=training)
+    concat2 = tf.concat([att2, deconv2], axis=-1, name='concat2')
+
+    conv8_1 = layers.conv2D_layer_bn(concat2, 'conv8_1', num_filters=n_filt * 2, training=training)
+    conv8_2 = layers.conv2D_layer_bn(conv8_1, 'conv8_2', num_filters=n_filt * 2, training=training)
+
+    deconv1 = layers.upsample(conv8_2)
+
+    att1 = layers.conv2D_layer_bn(conv1_2, 'att1', kernel_size=(1, 1), num_filters=n_filt, training=training)
+    concat1 = tf.concat([att1, deconv1], axis=-1, name='concat1')
+
+    conv9_1 = layers.conv2D_layer_bn(concat1, 'conv9_1', num_filters=n_filt, training=training)
+
+    deep2 = layers.upsample(conv7_2)
+    add2 = tf.concat([deep2, conv8_2], axis=-1, name='add2')
+    deep1 = layers.upsample(add2)
+    add1 = tf.concat([deep1, conv9_1], axis=-1, name='add1')
+
+    pred = layers.conv2D_layer_bn(add1, 'pred', num_filters=nlabels, kernel_size=(1, 1), activation=tf.identity,
+                                  training=training)
+
+    print('conv1_1', conv1_1.shape)
+    print('conv1_2', conv1_2.shape)
+    print('pool1', pool1.shape)
+    print('conv2_1', conv2_1.shape)
+    print('conv2_2', conv2_2.shape)
+    print('pool2', pool2.shape)
+    print('conv3_1', conv3_1.shape)
+    print('conv3_2', conv3_2.shape)
+    print('pool3', pool3.shape)
+    print('conv4_1', conv4_1.shape)
+    print('conv4_2', conv4_2.shape)
+    print('pool4', pool4.shape)
+    print('conv5_1', conv5_1.shape)
+    print('conv5_2', conv5_2.shape)
+    print('deconv4', deconv4.shape)
+    print('att4', att4.shape)
+    print('concat4', concat4.shape)
+    print('conv6_1', conv6_1.shape)
+    print('deconv3', deconv3.shape)
+    print('att3', att3.shape)
+    print('concat3', concat3.shape)
+    print('conv7_1', conv7_1.shape)
+    print('deconv2', deconv2.shape)
+    print('att2', att2.shape)
+    print('concat2', concat2.shape)
+    print('conv8_1', conv8_1.shape)
+    print('deconv1', deconv1.shape)
+    print('att1', att1.shape)
+    print('concat1', concat1.shape)
+    print('conv9_1', conv9_1.shape)
+    print('deep2', deep2.shape)
+    print('add2', add2.shape)
+    print('deep1', deep1.shape)
+    print('add1', add1.shape)
+    print('pred', pred.shape)
+
+    return pred
+
+def mod8unet2D(images, training, nlabels):
+    n_filt = 48
+    conv1_1 = layers.conv2D_layer_bn(images, 'conv1_1', num_filters=n_filt, training=training)
+    conv1_2 = layers.conv2D_layer_bn(conv1_1, 'conv1_2', num_filters=n_filt, training=training)
+
+    pool1 = layers.max_pool_layer2d(conv1_2)
+    pool1 = layers.dropout_layer(pool1, 'drop1', training=training, keep_prob=0.2)
+
+    conv2_1 = layers.conv2D_layer_bn(pool1, 'conv2_1', num_filters=n_filt * 2, training=training)
+    conv2_2 = layers.conv2D_layer_bn(conv2_1, 'conv2_2', num_filters=n_filt * 2, training=training)
+
+    pool2 = layers.max_pool_layer2d(conv2_2)
+    pool2 = layers.dropout_layer(pool2, 'drop2', training=training, keep_prob=0.2)
+
+    conv3_1 = layers.conv2D_layer_bn(pool2, 'conv3_1', num_filters=n_filt * 4, training=training)
+    conv3_2 = layers.conv2D_layer_bn(conv3_1, 'conv3_2', num_filters=n_filt * 4, training=training)
+
+    pool3 = layers.max_pool_layer2d(conv3_2)
+    pool3 = layers.dropout_layer(pool3, 'drop3', training=training, keep_prob=0.2)
+
+    conv4_1 = layers.conv2D_layer_bn(pool3, 'conv4_1', num_filters=n_filt * 8, training=training)
+    conv4_2 = layers.conv2D_layer_bn(conv4_1, 'conv4_2', num_filters=n_filt * 8, training=training)
+
+    pool4 = layers.max_pool_layer2d(conv4_2)
+    pool4 = layers.dropout_layer(pool4, 'drop4', training=training, keep_prob=0.2)
+
+    conv5_1 = layers.conv2D_layer_bn(pool4, 'conv5_1', num_filters=n_filt * 16, training=training)
+    conv5_2 = layers.conv2D_layer_bn(conv5_1, 'conv5_2', num_filters=n_filt * 16, training=training)
+
+    deconv4 = layers.upsample(conv5_2)
+
+    att4 = layers.attention(conv4_2, 'att4', conv5_2)
+    concat4 = tf.concat([att4, deconv4], axis=-1, name='concat4')
+
+    conv6_1 = layers.conv2D_layer_bn(concat4, 'conv6_1', num_filters=n_filt * 8, training=training)
+    conv6_2 = layers.conv2D_layer_bn(conv6_1, 'conv6_2', num_filters=n_filt * 8, training=training)
+
+    deconv3 = layers.upsample(conv6_2)
+
+    att3 = layers.attention(conv3_2, 'att3', conv6_1)
+    concat3 = tf.concat([att3, deconv3], axis=-1, name='concat3')
+
+    conv7_1 = layers.conv2D_layer_bn(concat3, 'conv7_1', num_filters=n_filt * 4, training=training)
+    conv7_2 = layers.conv2D_layer_bn(conv7_1, 'conv7_2', num_filters=n_filt * 4, training=training)
+
+    deconv2 = layers.upsample(conv7_2)
+
+    att2 = layers.conv2D_layer_bn(conv2_2, 'att2', kernel_size=(1, 1), num_filters=n_filt*2, training=training)
+    concat2 = tf.concat([att2, deconv2], axis=-1, name='concat2')
+
+    conv8_1 = layers.conv2D_layer_bn(concat2, 'conv8_1', num_filters=n_filt * 2, training=training)
+    conv8_2 = layers.conv2D_layer_bn(conv8_1, 'conv8_2', num_filters=n_filt * 2, training=training)
+
+    deconv1 = layers.upsample(conv8_2)
+
+    att1 = layers.conv2D_layer_bn(conv1_2, 'att1', kernel_size=(1, 1), num_filters=n_filt, training=training)
+    concat1 = tf.concat([att1, deconv1], axis=-1, name='concat1')
+
+    conv9_1 = layers.conv2D_layer_bn(concat1, 'conv9_1', num_filters=n_filt, training=training)
+    conv9_2 = layers.conv2D_layer_bn(conv9_1, 'conv9_2', num_filters=n_filt, training=training)
+
+    pred = layers.conv2D_layer_bn(conv9_2, 'pred', num_filters=nlabels, kernel_size=(1, 1), activation=tf.identity,
+                                  training=training)
+
+    print('conv1_1', conv1_1.shape)
+    print('conv1_2', conv1_2.shape)
+    print('pool1', pool1.shape)
+    print('conv2_1', conv2_1.shape)
+    print('conv2_2', conv2_2.shape)
+    print('pool2', pool2.shape)
+    print('conv3_1', conv3_1.shape)
+    print('conv3_2', conv3_2.shape)
+    print('pool3', pool3.shape)
+    print('conv4_1', conv4_1.shape)
+    print('conv4_2', conv4_2.shape)
+    print('pool4', pool4.shape)
+    print('conv5_1', conv5_1.shape)
+    print('conv5_2', conv5_2.shape)
+    print('deconv4', deconv4.shape)
+    print('att4', att4.shape)
+    print('concat4', concat4.shape)
+    print('conv6_1', conv6_1.shape)
+    print('deconv3', deconv3.shape)
+    print('att3', att3.shape)
+    print('concat3', concat3.shape)
+    print('conv7_1', conv7_1.shape)
+    print('deconv2', deconv2.shape)
+    print('att2', att2.shape)
+    print('concat2', concat2.shape)
+    print('conv8_1', conv8_1.shape)
+    print('deconv1', deconv1.shape)
+    print('att1', att1.shape)
+    print('concat1', concat1.shape)
+    print('conv9_1', conv9_1.shape)
+    print('pred', pred.shape)
+
+    return pred
+
+
+def mod9unet2D(images, training, nlabels):
+    n_filt = 48
+    conv1_1 = layers.conv2D_layer_bn(images, 'conv1_1', num_filters=n_filt, training=training)
+    conv1_2 = layers.conv2D_layer_bn(conv1_1, 'conv1_2', num_filters=n_filt, training=training)
+
+    pool1 = layers.max_pool_layer2d(conv1_2)
+
+    conv2_1 = layers.conv2D_layer_bn(pool1, 'conv2_1', num_filters=n_filt * 2, training=training)
+    conv2_2 = layers.conv2D_layer_bn(conv2_1, 'conv2_2', num_filters=n_filt * 2, training=training)
+
+    pool2 = layers.max_pool_layer2d(conv2_2)
+
+    conv3_1 = layers.conv2D_layer_bn(pool2, 'conv3_1', num_filters=n_filt * 4, training=training)
+    conv3_2 = layers.conv2D_layer_bn(conv3_1, 'conv3_2', num_filters=n_filt * 4, training=training)
+
+    pool3 = layers.max_pool_layer2d(conv3_2)
+
+    conv4_1 = layers.conv2D_layer_bn(pool3, 'conv4_1', num_filters=n_filt * 8, training=training)
+    conv4_2 = layers.conv2D_layer_bn(conv4_1, 'conv4_2', num_filters=n_filt * 8, training=training)
+
+    pool4 = layers.max_pool_layer2d(conv4_2)
+
+    conv5_1 = layers.conv2D_layer_bn(pool4, 'conv5_1', num_filters=n_filt * 16, training=training)
+    drop1 = layers.dropout_layer(conv5_1, 'drop1', training=training, keep_prob=0.2)
+    conv5_2 = layers.conv2D_layer_bn(drop1, 'conv5_2', num_filters=n_filt * 16, training=training)
+    drop2 = layers.dropout_layer(conv5_2, 'drop2', training=training, keep_prob=0.2)
+
+    deconv4 = layers.upsample(drop2)
+
+    att4 = layers.attention(conv4_2, 'att4', conv5_2)
+    concat4 = tf.concat([att4, deconv4], axis=-1, name='concat4')
+
+    conv6_1 = layers.conv2D_layer_bn(concat4, 'conv6_1', num_filters=n_filt * 8, training=training)
+    conv6_2 = layers.conv2D_layer_bn(conv6_1, 'conv6_2', num_filters=n_filt * 8, training=training)
+
+    deconv3 = layers.upsample(conv6_2)
+
+    att3 = layers.attention(conv3_2, 'att3', conv6_1)
+    concat3 = tf.concat([att3, deconv3], axis=-1, name='concat3')
+
+    conv7_1 = layers.conv2D_layer_bn(concat3, 'conv7_1', num_filters=n_filt * 4, training=training)
+    conv7_2 = layers.conv2D_layer_bn(conv7_1, 'conv7_2', num_filters=n_filt * 4, training=training)
+
+    deconv2 = layers.upsample(conv7_2)
+
+    att2 = layers.conv2D_layer_bn(conv2_2, 'att2', kernel_size=(1, 1), num_filters=n_filt*2, training=training)
+    concat2 = tf.concat([att2, deconv2], axis=-1, name='concat2')
+
+    conv8_1 = layers.conv2D_layer_bn(concat2, 'conv8_1', num_filters=n_filt * 2, training=training)
+    conv8_2 = layers.conv2D_layer_bn(conv8_1, 'conv8_2', num_filters=n_filt * 2, training=training)
+
+    deconv1 = layers.upsample(conv8_2)
+
+    att1 = layers.conv2D_layer_bn(conv1_2, 'att1', kernel_size=(1, 1), num_filters=n_filt, training=training)
+    concat1 = tf.concat([att1, deconv1], axis=-1, name='concat1')
+
+    conv9_1 = layers.conv2D_layer_bn(concat1, 'conv9_1', num_filters=n_filt, training=training)
+    conv9_2 = layers.conv2D_layer_bn(conv9_1, 'conv9_2', num_filters=n_filt, training=training)
+
+    pred = layers.conv2D_layer_bn(conv9_2, 'pred', num_filters=nlabels, kernel_size=(1, 1), activation=tf.identity,
+                                  training=training)
+
+    return pred
